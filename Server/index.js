39122044bdc2eb39d9a683340012dba2aff9d97c
Kumar Anand <kranand9430@gmail.com>
2026-01-21 00:21:59 +0530

    server setup

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express"
+import http from "http"
+import cors from "cors"
 
-app.get('/', (req, res) => {
+//Create Express and Http server
+const app = express()
+const server = http.createServer(app)
+const PORT = process.env.PORT || 3000
+
+
+//Creating middlewares
+app.use(express.json({ limit: '5mb' }))
+app.use(cors())
+
+
+//Sample Route
+app.use('/api/status', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+server.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
